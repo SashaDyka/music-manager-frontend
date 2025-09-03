7f3400188535ecec63223d86409bf8f0116900da
SashaDyka <beli.rothi2005@gmail.com>
2025-09-03 23:53:06 +0300

    feat: edit constructor

--- src/controllers/appController.ts
+++ src/controllers/appController.ts
@@ -1,5 +1,5 @@
 import { DataService } from '../services/dataService.js';
-import { MainContentController } from './MainContentController.js';
+import { MainContentController } from './mainContentController.js';
 import { PlaylistReviewView } from '../views/playlistReviewView.js';
 import { PlayerController } from './playerController.js';
 import { MainContentView } from '../views/mainContentView.js';
@@ -7,23 +7,28 @@ import { MainContentView } from '../views/mainContentView.js';
 export class AppController {
     private dataService: DataService;
     private mainContentController: MainContentController;
+    private playlistReviewView: PlaylistReviewView;
     private playerController: PlayerController;
+
 
     constructor() {
         this.dataService = new DataService();
         const mainContentView = new MainContentView();
         const playlistReviewView = new PlaylistReviewView();
+        this.playerController = new PlayerController();
         
-        this.mainContentController = new MainContentController(this.dataService, mainContentView, playlistReviewView);
+        this.mainContentController = new MainContentController(
+            this.dataService, 
+            mainContentView, 
+            playlistReviewView,
+            this.playerController 
+        );
     }
 
     public async init(): Promise<void> {
         console.log('App is starting...');
         try {
-            const currentUserId = 1;
-            const currentUser = await this.dataService.getCurrentUser(currentUserId);
             const userPlaylists = await this.dataService.getPlaylistsByUserId(currentUserId);
-            
             this.mainContentController.loadAndDisplayPlaylists(userPlaylists);
 
             console.log('App initialized successfully.');
