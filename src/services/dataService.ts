e15976b63d3b9053b1ebc31dceaa75423275f18c
SashaDyka <beli.rothi2005@gmail.com>
2025-09-03 19:49:39 +0300

    feat: create base async functions

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -1,4 +1,7 @@
 import type  { User } from '../models/userModel.js';
+import type { Song } from '../models/songModel.js';
+import type { Playlist } from '../models/playlistModel.js';
+
 
 export class DataService {
     private readonly API_URL = 'http://localhost:3000';
@@ -30,4 +33,48 @@ export class DataService {
             throw error;
         }
     }
+
+    public async getUserPlaylists(userId: number): Promise<Playlist[]> {
+    try {
+        const response = await fetch(`${this.API_URL}/users/${userId}/playlists`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data: Playlist[] = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error fetching playlists for user ${userId}:`, error);
+        throw error;
+    }
+}
+
+
+public async getPlaylistById(playlistId: number): Promise<Playlist> {
+    try {
+        const response = await fetch(`${this.API_URL}/playlists/${playlistId}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data: Playlist = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error fetching playlist with ID ${playlistId}:`, error);
+        throw error;
+    }
+}
+
+public async getPlaylistsByUserId(userId: number): Promise<Playlist[]> {
+        try {
+            const response = await fetch(`${this.API_URL}/users/${userId}/playlists`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data: Playlist[] = await response.json();
+            return data;
+        } catch (error) {
+            console.error(`Failed to fetch playlists for user ${userId}:`, error);
+            throw error;
+        }
+    }
+
 }
